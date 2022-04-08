2f19531f5fff91edee3ec99bd6998cdaf016070e
Rezoan Shakil Prince <prince.cuet.77@gmail.com>
2022-04-08 17:35:28 +0600

    Initial commit. Store that into the github repos

---
+++ routing/EpidemicRouter.java
@@ -0,0 +1,60 @@
+/* 
+ * Copyright 2010 Aalto University, ComNet
+ * Released under GPLv3. See LICENSE.txt for details. 
+ */
+package routing;
+
+import core.Settings;
+
+/**
+ * Epidemic message router with drop-oldest buffer and only single transferring
+ * connections at a time.
+ */
+public class EpidemicRouter extends ActiveRouter {
+	
+	/**
+	 * Constructor. Creates a new message router based on the settings in
+	 * the given Settings object.
+	 * @param s The settings object
+	 */
+	public EpidemicRouter(Settings s) {
+		super(s);
+		System.out.println("Constructor calling settings constructor"); // M
+		//TODO: read&use epidemic router specific settings (if any)
+	}
+	
+	/**
+	 * Copy constructor.
+	 * @param r The router prototype where setting values are copied from
+	 */
+	protected EpidemicRouter(EpidemicRouter r) {
+		super(r);
+		System.out.println("Constructor calling Epidemic Router constructor"); // M
+		//TODO: copy epidemic settings here (if any)
+	}
+			
+	@Override
+	public void update() {
+		super.update();
+		System.out.println("Update method from EpidemicRouter class"); // M
+		if (isTransferring() || !canStartTransfer()) {
+			return; // transferring, don't try other connections yet
+		}
+		
+		// Try first the messages that can be delivered to final recipient
+		if (exchangeDeliverableMessages() != null) {
+			return; // started a transfer, don't try others (yet)
+		}
+		
+		// then try any/all message to any/all connection
+		this.tryAllMessagesToAllConnections();
+	}
+	
+	
+	@Override
+	public EpidemicRouter replicate() {
+		System.out.println("replicate method from EpidemicRouter class"); // M
+		return new EpidemicRouter(this);
+	}
+
+}
